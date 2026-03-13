5e53516744aaff247d8998ed5716ea456a627fcd
anurag-yadav-1619 <anuragyadava07277@gmail.com>
2026-03-14 00:56:42 +0530

    anurag

---
+++ arrayrepresantation.cpp
@@ -0,0 +1,16 @@
+
+# include <iostream>
+using namespace std;
+int main(){
+    int arr[5];
+    arr[0]=4;
+    arr[1]=56;
+    arr[2]=3;
+    arr[3]=7;
+    arr[4]=2;
+    cout<<arr[4]<<endl;
+    arr[4]=3;
+    cout<<arr[4]<<endl;
+
+    return 0;
+}
