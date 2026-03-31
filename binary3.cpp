12d2bdb7437e14dc5e6eea778da6609773844fc8
anurag-yadav-1619 <anuragyadava07277@gmail.com>
2026-03-31 23:38:16 +0530

    cpp

--- binary3.cpp
+++ binary3.cpp
@@ -1,37 +1,3 @@
-# include <iostream>
-#include<vector>
-using namespace std;
-int anurag(vector<int> arr,int tar){
-    int st=0,end=arr.size()-1;
-    while (st<=end){
-        int mid=(st+end)/2;
-        if(tar>arr[mid]){
-            st=mid+1;
-        }
-        else if(tar<arr[mid]){
-            end=mid-1;
-        }
-        else{
-            return mid;
-        }
-    }
-    return -1;
-}
-
-p-      
-
-
-int main(){
-    vector<int> arr1={-1,0,3,4,5,9,12};
-    int tar1=0;
-    cout<< anurag(arr1,tar1)<<endl;
-    vector<int> arr2={-1,0,3,4,5,9,12};
-    int tar2=7;
-    cout<< anurag(arr2,tar2)<<endl;
-
-    
-    return 0;
-}
 #include <iostream>
 #include <algorithm> // for sort()
 using namespace std;
---
+++ revercearray.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+int main(){
+    
+    int arr[5]={4,5,6,7,8};
+    for(int i=4;i>=0;i--){
+        cout<<arr[i];
+    }
+    for(int i=1;i=1;i++){
+        cout<<arr[i]<<endl;
+    }
+    // cout<<arr[1];
+
+    return 0;
+}
